432a37daeebf9ab0b98d8b16d5d4d2c905bd91f4
Kim Jongchae <lhope2k@naver.com>
2014-12-18 16:48:24 +0900

    Add Unit Test

---
+++ TestUnitTestSrc/Popup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestUnitTestSrc
+{
+    public class Popup
+    {
+        public void Pop()
+        {
+            System.Windows.Forms.MessageBox.Show("Pop");
+        }
+
+        public void Pop2()
+        {
+            System.Windows.Forms.MessageBox.Show("Pop2");
+        }
+    }
+}
